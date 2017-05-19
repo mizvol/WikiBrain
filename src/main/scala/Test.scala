76efedd69f26ff1b5a9baa906d2d195ab161d192
Volodymyr Miz <volodymyr.miz@epfl.ch>
2017-05-19 09:06:39 +0200

    test

--- src/main/scala/Test.scala
+++ src/main/scala/Test.scala
@@ -1,10 +1,6 @@
 import ch.epfl.lts2.Utils._
 import org.apache.spark.graphx.{Edge, Graph, VertexId}
-import org.apache.spark.sql.{Row, SparkSession}
-import java.security.MessageDigest
-
-import org.apache.spark.sql.types._
-
+import org.apache.spark.sql.SparkSession
 
 /**
   * Created by volodymyrmiz on 30.04.17.
@@ -14,12 +10,15 @@ object Test {
     suppressLogs(List("org", "akka"))
 
     val spark = SparkSession.builder
-      .master("local")
+      .master("local[*]")
       .appName("Test")
+      .config("spark.driver.maxResultSize", "2g")
+      .config("spark.executor.memory", "50g")
       .getOrCreate()
 
-    val sc = spark.sparkContext
-    import spark.sqlContext.implicits._
+//    val sc = spark.sparkContext
+//    implicit val ctx:SparkContext = spark.sparkContext
+
     //    val path: String = "./src/main/resources/wikiTS/"
     //    val idsfileName: String = "ids_titles_for_20000_filtered.csv"
     //
@@ -30,11 +29,13 @@ object Test {
     //
     //    val idsTitlesMap = idsDF.collect.map(pair => pair{0} -> pair{1}).toMap
 
-
-//    val vertices =
-//      sc.parallelize(Seq[(VertexId, String)]((1, "one"), (2, "two"), (3, "three")), 2)
-//    val edges = sc.parallelize((Seq(Edge(1, 2, true), Edge(2, 3, false), Edge(3, 1, false))))
-//    val g: Graph[String, Boolean] = Graph(vertices, edges)
+    /**
+      * Coarsening test @author Dave Ankur
+      */
+    val verticesRDD = spark.sparkContext.parallelize(Seq[(VertexId, String)]((1, "one"), (2, "two"), (3, "three")), 2)
+    val edgesRDD = spark.sparkContext.parallelize((Seq(Edge(1, 2, true), Edge(2, 3, false), Edge(3, 1, false), Edge(10,11, false))))
+    val g: Graph[String, Boolean] = Graph(verticesRDD, edgesRDD)
+    println(g.edges.count())
 //
 //    val c = g.coarsen(_.attr, _ + _).cache()
 //    val cV = c.vertices.collect.toSet
@@ -44,30 +45,85 @@ object Test {
 //
 //    println("End")
 
-    val str = List ("String", "Another_string", "String,", "String", "String")
 
-    def md5(s: String) = {
-      MessageDigest.getInstance("MD5").digest(s.getBytes)
-    }
+    /**
+      * Load wiki page counts into a Spark DF
+      */
+//    val str = List ("String", "Another_string", "String,", "String", "String")
+//
+//    def md5(s: String) = {
+//      MessageDigest.getInstance("MD5").digest(s.getBytes)
+//    }
+//
+//
+//    for (s <- str) {
+//      println(s.hashCode)
+//
+//    }
+//
+//    val path: String = "./src/main/resources/wikiTS/"
+//
+//    val wikiData = sc.textFile(path + "pagecounts-20160101-000000.gz").map(_.split(" ")).map(row => Row(row(0), row(1), row(2), row(3)))
+//
+//    val schemaString = "lang pageName pageCount size"
+//
+//    val fields = schemaString.split(" ")
+//      .map(fieldName => StructField(fieldName, StringType, nullable = true))
+//    val schema = StructType(fields)
+//
+//    val wikiDataDF = spark.createDataFrame(wikiData, schema)
+//
+//    wikiDataDF.filter("lang = 'en'").filter("pageCount > 100").show()
+
+    /**
+      * Load GraphML to GraphX. Note: Change ID type from LONG to INT before loading, since LONG is not supported
+      */
+
+//    val pathSSD = "/home/volodymyrmiz/wikiGraphML.xml"
+
+//    implicit val ctx: SparkContext = spark.sparkContext
+//
+//    val graph = LoadGraph.from(GraphML(pathSSD)).load()
+//
+//    Path("/home/volodymyrmiz/vertices").deleteRecursively()
+//    graph.vertices.saveAsObjectFile("/home/volodymyrmiz/vertices")
+//    Path("/home/volodymyrmiz/edges").deleteRecursively()
+//    graph.edges.saveAsObjectFile("/home/volodymyrmiz/edges")
 
 
-    for (s <- str) {
-      println(s.hashCode)
+    /**
+      * Load graph from file
+      */
+//    val vertices: RDD[(VertexId, Map[String, Int])] = spark.sparkContext.objectFile("/home/volodymyrmiz/vertices")
+//    val edges: RDD[Edge[Map[String, String]]] = spark.sparkContext.objectFile("/home/volodymyrmiz/edges")
+//
+//    val graph1 = Graph(vertices, edges)
+//    println(graph1.vertices.take(10).mkString("\n"))
+//    println(graph1.edges.take(10).mkString("\n"))
 
-    }
 
-    val path: String = "./src/main/resources/wikiTS/"
+    /**
+      * Load graph from csv
+      */
 
-    val wikiData = sc.textFile(path + "pagecounts-20160101-000000.gz").map(_.split(" ")).map(row => Row(row(0), row(1), row(2), row(3)))
-
-    val schemaString = "lang pageName pageCount size"
-
-    val fields = schemaString.split(" ")
-      .map(fieldName => StructField(fieldName, StringType, nullable = true))
-    val schema = StructType(fields)
-
-    val wikiDataDF = spark.createDataFrame(wikiData, schema)
-
-    wikiDataDF.filter("lang = 'en'").filter("pageCount > 100").show()
+//    val path = "/mnt/data/git/wiki/notebook/"
+//
+//    val vertices = spark.sqlContext.read
+//      .format("com.databricks.spark.csv")
+//      .option("header", "false")
+//      .option("inferSchema", "false")
+//      .option("delimiter", ",")
+//      .load(path + "vertices.csv")
+//
+//    vertices.show()
+//
+//    val edges = spark.sqlContext.read
+//      .format("com.databricks.spark.csv")
+//      .option("header", "false")
+//      .option("inferSchema", "false")
+//      .option("delimiter", " ")
+//      .load(path + "edges.csv")
+//
+//    edges.show()
   }
 }
