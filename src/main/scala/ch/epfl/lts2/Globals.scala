166f879511d8dd81141e9f5d9c9caeea3c1f528c
Volodymyr Miz <volodymyr.miz@epfl.ch>
2017-05-19 09:07:01 +0200

    Added global variables

--- src/main/scala/ch/epfl/lts2/Globals.scala
+++ src/main/scala/ch/epfl/lts2/Globals.scala
@@ -1,6 +1,8 @@
+package ch.epfl.lts2
+
 /**
   * Created by volodymyrmiz on 15.05.17.
   */
-object Globals {
-
+package object Globals {
+  val PATH_RESOURCES: String = "/mnt/data/git/WikiBrain/src/main/resources/"
 }
