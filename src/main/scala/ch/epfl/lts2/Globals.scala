a1c5b18a78ae6259755a314277bc5eaca4bae6dc
Volodymyr Miz <volodymyr.miz@epfl.ch>
2017-05-26 13:02:28 +0200

    Added dates in relative hours

--- src/main/scala/ch/epfl/lts2/Globals.scala
+++ src/main/scala/ch/epfl/lts2/Globals.scala
@@ -5,4 +5,29 @@ package ch.epfl.lts2
   */
 package object Globals {
   val PATH_RESOURCES: String = "/mnt/data/git/WikiBrain/src/main/resources/"
+  val TOTAL_HOURS: Int = 5278
+
+  val OCT_START = 0
+  val OCT_END = 912
+
+  val NOV_START = 913
+  val NOV_END = 1633
+
+  val DEC_START = 1634
+  val DEC_END = 2354
+
+  val JAN_START = 2355
+  val JAN_END = 3074
+
+  val FEB_SRART = 3075
+  val FEB_END = 3795
+
+  val MAR_START = 3796
+  val MAR_END = 4514
+
+  val APR_START = 4515
+  val APR_END = TOTAL_HOURS
+
+  val MONTH_HOURS = 744
+
 }
