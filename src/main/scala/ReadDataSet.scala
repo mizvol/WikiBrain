c84361e542a2bbc12c46ec50e437713639d7ee52
Volodymyr Miz <volodymyr.miz@epfl.ch>
2017-05-27 13:03:32 +0200

    Code cleanup

--- src/main/scala/ReadDataSet.scala
+++ src/main/scala/ReadDataSet.scala
@@ -31,12 +31,12 @@ object ReadDataSet {
   private def readLayeredSignal(path: String, spark: SparkSession) = {
     val df = spark.sqlContext.read
       .format("com.databricks.spark.csv")
-      .options(Map("header"->"true", "inferSchema"->"true"))
+      .options(Map("header" -> "true", "inferSchema" -> "true"))
       .load(path)
       .drop("_c0")
 
-    val rdd = df.toJavaRDD.rdd.map(_.toSeq.toList).map(page => (page.head.toString.toLong, (page{2}, page{1}))).groupBy(_._1)
-      .mapValues(page => page.map(_._2).groupBy(_._1).map{case(k,v) => (k, v.map(_._2))})
+    val rdd = df.toJavaRDD.rdd.map(_.toSeq.toList).map(page => (page.head.toString.toLong, (page {2}, page {1}))).groupBy(_._1)
+      .mapValues(page => page.map(_._2).groupBy(_._1).map { case (k, v) => (k, v.map(_._2)) })
       .mapValues(pair => (pair.keys.map(_.toString.toInt).toArray, pair.values.map(_.head.toString.toDouble).toArray))
       .mapValues(pair => Vectors.sparse(pair._1.length, pair._1, pair._2).toSparse)
       .mapValues(visitsTS => visitsTS.indices.zip(visitsTS.values).toMap)
@@ -48,10 +48,10 @@ object ReadDataSet {
   private def readTabularSignal(path: String, spark: SparkSession) = {
     val df = spark.sqlContext.read
       .format("com.databricks.spark.csv")
-      .options(Map("header"->"true", "inferSchema"->"true"))
+      .options(Map("header" -> "true", "inferSchema" -> "true"))
       .load(path)
 
-    val rdd = df.rdd.map(v => (v{0}.toString.toLong, v.toSeq.toList.drop(1).map(_.toString.toDouble))).mapValues(v => (v.indices.map(_ + 1) zip v).toMap.filter(_._2 != 0))
+    val rdd = df.rdd.map(v => (v {0}.toString.toLong, v.toSeq.toList.drop(1).map(_.toString.toDouble))).mapValues(v => (v.indices.map(_ + 1) zip v).toMap.filter(_._2 != 0))
 
     Path(PATH_RESOURCES + "RDDs/tabularSignalRDD").deleteRecursively()
     rdd.saveAsObjectFile(PATH_RESOURCES + "RDDs/tabularSignalRDD")
