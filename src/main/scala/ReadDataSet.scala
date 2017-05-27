02ca07c8b47e485819955c7428eb2a4e61718f8e
Volodymyr Miz <volodymyr.miz@epfl.ch>
2017-05-27 13:04:34 +0200

    Read static graph + timeseries and merge the two

--- src/main/scala/ReadDataSet.scala
+++ src/main/scala/ReadDataSet.scala
@@ -1,6 +1,8 @@
 import ch.epfl.lts2.Utils._
 import ch.epfl.lts2.Globals._
+import org.apache.spark.graphx.VertexId
 import org.apache.spark.mllib.linalg.Vectors
+import org.apache.spark.rdd.RDD
 import org.apache.spark.sql.SparkSession
 
 import scala.reflect.io.Path
@@ -19,13 +21,16 @@ object ReadDataSet {
       .config("spark.executor.memory", "50g")
       .getOrCreate()
 
-    val path: String = "./src/main/resources/wikiTS/"
     val fileNameLayered: String = "signal_500.csv"
     val fileNameTabular: String = "activations_100.csv"
+    val pathToCSV: String = PATH_RESOURCES + "csv/"
+    val pathToTimeSeries: String = PATH_RESOURCES + "wikiTS/"
 
-    readLayeredSignal(path + fileNameLayered, spark)
+    readLayeredSignal(pathToTimeSeries + fileNameLayered, spark)
 
-    readTabularSignal(path + fileNameTabular, spark)
+    readTabularSignal(pathToTimeSeries + fileNameTabular, spark)
+
+    readStaticGraph(pathToCSV, pathToTimeSeries, spark)
   }
 
   private def readLayeredSignal(path: String, spark: SparkSession) = {
@@ -55,5 +60,57 @@ object ReadDataSet {
 
     Path(PATH_RESOURCES + "RDDs/tabularSignalRDD").deleteRecursively()
     rdd.saveAsObjectFile(PATH_RESOURCES + "RDDs/tabularSignalRDD")
+  }
+
+  private def readStaticGraph(pathToCSV: String, pathToTimeSeries: String, spark: SparkSession) = {
+
+    import spark.implicits._
+
+    val verticesDF = spark.sqlContext.read
+      .format("com.databricks.spark.csv")
+      .option("header", "false")
+      .option("inferSchema", "false")
+      .option("delimiter", ",")
+      .load(pathToCSV + "vertices.csv")
+
+    println("Vertices initially: " + verticesDF.count())
+
+    val edgesDF = spark.sqlContext.read
+      .format("com.databricks.spark.csv")
+      .option("header", "false")
+      .option("inferSchema", "false")
+      .option("delimiter", " ")
+      .load(pathToCSV + "/edges.csv")
+
+    println("Edges initially: " + edgesDF.count())
+
+    val timeSeriesDF = spark.sqlContext.read
+      .format("com.databricks.spark.csv")
+      .options(Map("header" -> "true", "inferSchema" -> "true"))
+      .load(pathToTimeSeries + "signal_500.csv")
+      .drop("_c0")
+      .rdd
+      .map(_.toSeq.toList).map(page => (page.head.toString.toLong, (page {2}, page {1}))).groupBy(_._1)
+      .mapValues(page => page.map(_._2).groupBy(_._1).map { case (k, v) => (k, v.map(_._2)) })
+      .mapValues(pair => (pair.keys.map(_.toString.toInt).toArray, pair.values.map(_.head.toString.toDouble).toArray))
+      .mapValues(pair => Vectors.sparse(pair._1.length, pair._1, pair._2).toSparse)
+      .mapValues(visitsTS => visitsTS.indices.zip(visitsTS.values).toMap)
+      .toDF(Seq("_c0", "_c1"): _*)
+
+    println("Vertices with timeseries: " + timeSeriesDF.count())
+
+    val verticesTimeSeriesDF = verticesDF.join(timeSeriesDF, Seq("_c0"), "outer").toDF(Seq("id", "title", "ts"): _*).filter("ts is not null")
+
+    println("Vertices merged: " + verticesTimeSeriesDF.count())
+
+    val verticesRDD: RDD[(VertexId, (String, Map[Int, Double]))] = verticesTimeSeriesDF
+      .as[(String, String, Map[Int, Double])]
+      .rdd
+      .filter(v => isAllDigits(v._1))
+      .filter(v => v._2 != null)
+      .map(v => (v._1.toLong, (v._2.replace("&", "").replace("""\"""", ""), v._3)))
+
+    Path(PATH_RESOURCES + "RDDs/staticVerticesRDD").deleteRecursively()
+    verticesRDD.saveAsObjectFile(PATH_RESOURCES + "RDDs/staticVerticesRDD")
   }
 }
